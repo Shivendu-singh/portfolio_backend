4ea756aa72621a68960b01008ebc3aa4c106a24a
Shivendu-singh <shivendusingh98351@gmail.com>
2026-07-03 12:59:32 +0530

    style: admin interface update using django-unfold

--- core_project/settings.py
+++ core_project/settings.py
@@ -11,6 +11,7 @@ DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 INSTALLED_APPS = [
+    "unfold",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
